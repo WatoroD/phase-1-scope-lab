786509942c5ac00d4f4e33b6c66e35fa8489221e
Daniel Watoro <watorondirangu@gmail.com>
2024-06-30 20:49:07 +0300

    Adds new code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'a';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = undefined;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
+
+console.log(customerName);
+setBestCustomer();
+console.log(bestCustomer);
+overwriteBestCustomer();
+console.log(bestCustomer);
+try {
+  changeLeastFavoriteCustomer();
+} catch (e) {
+  console.log(e.message);
+}
+console.log(leastFavoriteCustomer);
