e691c255a7724feed831dafded3da51a14ee19d1
Daniel Watoro <watorondirangu@gmail.com>
2024-07-17 19:14:49 +0300

    Adds new code

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 var customerName = 'bob';
-var bestCustomer;
-const leastFavoriteCustomer = 'a';
-
-function upperCaseCustomerName() {
+function upperCaseCustomerName(){
   customerName = customerName.toUpperCase();
 }
-
-function setBestCustomer() {
-  bestCustomer = undefined;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
 }
-
-function overwriteBestCustomer() {
-  bestCustomer = 'maybe bob';
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
 }
-
-function changeLeastFavoriteCustomer() {
-  throw new Error("Assignment to constant variable.");
+const leastFavoriteCustomer = 'anyone'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'anyone';
 }
-
-console.log(customerName);
-setBestCustomer();
-console.log(bestCustomer);
-overwriteBestCustomer();
-console.log(bestCustomer);
-try {
-  changeLeastFavoriteCustomer();
-} catch (e) {
-  console.log(e.message);
-}
-console.log(leastFavoriteCustomer);
